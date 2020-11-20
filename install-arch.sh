eeb4415a802190a261ded82c6d2e0c4bbe0a15ef
Chaise Farrar <Chaise Farrar>
2020-11-20 07:01:12 -0600

    Added an arch version of the installer

---
+++ install-arch.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+sudo pacman -Syu
+sudo pacman -S zsh ccache neovim stow htop curl mpv cmatrix \
+    clang gcc cmake make ninja \
+
+stow -R zsh
+stow -R nvim
+stow -R regolith
+
+mkdir -p ./antibody
+curl -sfL git.io/antibody | sh -s - -b ~/dotfiles/antibody
+chmod +x ~/dotfiles/antibody/antibody
+~/dotfiles/antibody/antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#git submodule init
+#git submodule sync
+#git submodule update
+#git submodule foreach git checkout master
+#git submodule foreach git pull
